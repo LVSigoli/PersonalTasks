0e8fe1c8e8b0b5b38baf756a48b90bb533e00ce1
LVSigoli <lucasviniciussigoli@gmail.com>
2025-05-20 14:31:36 -0300

    chore: Add configs and libraries to project

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,4 +45,10 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    implementation(libs.androidx.room.runtime)
+    implementation(libs.androidx.room.ktx)
+
+
+
 }
